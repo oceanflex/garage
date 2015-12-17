2c3b86c363408164d90a79f0b4703bbe3d6b220b
Zachary <Zachary@ZCS-ASUS>
2015-12-17 13:33:43 -0600

    added doumentation to fileService package

--- src/fileService/TextFileReader.java
+++ src/fileService/TextFileReader.java
@@ -9,28 +9,43 @@ import java.io.IOException;
 import java.util.Map;
 
 /**
- *
+ *The FileReader is designed to read a file, ask the formatter to decode it,
+ * then return an array of maps to the service
  * @author zsummers
  */
 public class TextFileReader {
     private Formatter fmt;
     private String filePath;
-
+/**
+ * the TextFileReader allways needs a format and a filePath
+ * @param fmt the format for decoding files
+ * @param filePath the path for reading the file from
+ */
     public TextFileReader(Formatter fmt, String filePath) {
         this.fmt = fmt;
         this.filePath = filePath;
     }
-
+/**
+ * if the format needed to decode file data changes, this can be used to adjust it
+ * @param fmt the new format for reading files
+ */
     public void setFmt(Formatter fmt) {
         //needs validation
         this.fmt = fmt;
     }
-
+/**
+ * If the file that needs reading changes, this method can adjust it
+ * @param filePath the path to find the new file
+ */
     public void setFilePath(String filePath) {
         //needs validation
         this.filePath = filePath;
     }
-    
+    /**
+     * this method actually reads the file in, and decodes it
+     * @return a Map[] with strings as keys, as defined by the formatter
+     * @throws IOException if the file isn't found
+     */
     public Map<String, Object>[] readFile() throws IOException{
         File data = new File(filePath);
         BufferedReader in;
--- src/fileService/TextFileWriter.java
+++ src/fileService/TextFileWriter.java
@@ -6,28 +6,44 @@ import java.io.PrintWriter;
 import java.util.Map;
 
 /**
- *
+ *The FileWriter is designed to take an input array of maps, ask the formatter
+ * to encode the data into a string, then write the string to the spesified filePath
  * @author zsummers
  */
 public class TextFileWriter {
     private Formatter fmt;
     private String filePath;
-
+/**
+ * TextFileReader allways needs to know how to encode ans where to write data to
+ * @param fmt the format for encoding
+ * @param filePath the path to write the data to
+ */
     public TextFileWriter(Formatter fmt, String filePath) {
         this.fmt = fmt;
         this.filePath = filePath;
     }
-
+/**
+ * if the encoder format ever needs to be changed, this method can alter it
+ * @param fmt the new format to be used
+ */
     public void setFmt(Formatter fmt) {
         //needs validation
         this.fmt = fmt;
     }
-
+/**
+ * if the location to write the data to changes, this method will set it
+ * @param filePath - the new path to write the file to
+ */
     public void setFilePath(String filePath) {
         //needs validation
         this.filePath = filePath;
     }
-    
+    /**
+     * this is the method that actually will write the data to the file, after
+     * having the formatter encode the data
+     * @param toWrite - the data, in a map of arrays, with strings as keys, formatted as spesified by the formatter
+     * @throws FileNotFoundException if the filePath's directories don't exist
+     */
     public void writeFile(Map<String, Object>[] toWrite) throws FileNotFoundException{
         PrintWriter writer = new PrintWriter(filePath);
         try {
